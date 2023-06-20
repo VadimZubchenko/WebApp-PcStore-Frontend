bad2417e1befdf77901c2fd2427fb356bc065048
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2023-06-20 14:45:50 +0300

    refactored axios requests using error msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListCustomerComponent from "./components/ListCustomerComponent";
+import AddCustomerForm from "./components/AddCustomerForm";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <HeaderComponent />
+        <nav style={{ margin: 10 }}>
+          <Link to="/" style={{ padding: 5 }}>
+            Home
+          </Link>
+          <Link to="customers" style={{ padding: 5 }}>
+            Customers List
+          </Link>
+          <Link to="/add-customer" style={{ padding: 5 }}>
+            Add Customer
+          </Link>
+        </nav>
         <div className="container">
           <Routes>
-            <Route path="/" element={<ListCustomerComponent />}></Route>
-            <Route
-              path="/customers"
-              element={<ListCustomerComponent />}
-            ></Route>
+            <Route path="/" exact element={<ListCustomerComponent />} />
+            <Route path="/customers" element={<ListCustomerComponent />} />
+            <Route path="/add-customer" element={<AddCustomerForm />} />
+            <Route path="/*" element={<NoMatch />} />
           </Routes>
         </div>
         <FooterComponent />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
